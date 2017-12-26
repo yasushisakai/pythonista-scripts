473d85a970a5b2c5b0bf6811427dd717282ef56d
Yasushi Sakai <yasushisakai@users.noreply.github.com>
2017-12-25 19:14:42 -0500

    getting geolocation

--- scripts/test.py
+++ scripts/test.py
@@ -1,3 +1,14 @@
-# test.py
+# testing if pythonista lets me get the latlng
 
-print("hello")
+
+import location
+
+if location.is_authorized():
+	
+	location.start_updates()
+	latLng = location.get_location()
+	print(latLng)
+	location.stop_updates()
+	
+else :
+	print('location needs to be allowed')
