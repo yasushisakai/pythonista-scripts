be8d18fbac44b378a8d80a4d989ad31e1f16acd2
Yasushi Sakai <ysshski@gmail.com>
2018-03-02 14:34:00 -0500

    urllib

--- scripts/test.py
+++ scripts/test.py
@@ -2,13 +2,18 @@
 
 
 import location
+from urllib import request
 
 if location.is_authorized():
-	
-	location.start_updates()
-	latLng = location.get_location()
-	print(latLng)
-	location.stop_updates()
-	
-else :
-	print('location needs to be allowed')
+
+    location.start_updates()
+    latLng = location.get_location()
+    print(latLng)
+    location.stop_updates()
+
+else:
+    print('location needs to be allowed')
+
+response = request.urlopen('https://cityio.media.mit.edu')
+
+print(response)
